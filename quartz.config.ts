8209811cc4a5c5e02cd72eaa35255d76eb0dd5b7
seiya <sabaranran@gmail.com>
2026-03-08 13:15:37 +0900

    spa

--- quartz.config.ts
+++ quartz.config.ts
@@ -10,7 +10,7 @@ const config: QuartzConfig = {
   configuration: {
     pageTitle: "Sabaranran",
     pageTitleSuffix: "",
-    enableSPA: true,
+    enableSPA: false,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
